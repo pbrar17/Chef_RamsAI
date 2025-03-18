b78f72b031018b9f57e77929f0d02d530a65f99b
Pahul Brar <pahul.brar@gettyimages.com>
2025-03-18 13:24:30 -0600

    Phase 1 Complete: Added some css, added a logo, added a custom loader, next steps, might be to make the ai be more like chef ramsay, maybe add acounts so users can store their recipes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-// import chefClaudeLogo from "./images/chef-claude-icon.png"
+import chefLogo from "../../public/chefLogo.gif";
+import Image  from "next/image";
 
 export default function Header() {
     return (
         <header>
-            {/* <img src={chefClaudeLogo}/> */}
-            <h1>Chef Claude</h1>
+            {/* <img src={chefLogo}/> */}
+            <Image src={chefLogo} alt="Logo" width={150} height={50} priority />
+            <h1>Chef RamsAI</h1>
         </header>
     )
 }
--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,9 +1,17 @@
+import {useState} from 'react';
 export default function IngredientsList(props) {
     const ingredientsListItems = props.ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
+    const [loading, setLoading] = useState(false);
+
+    const handleGetRecipe = async () => {
+        setLoading(true);
+        await props.getRecipe();
+        setLoading(false);
+    }
     return (
-        <section>
+        <section className='ingredients-section'>
             <h2>Ingredients on hand:</h2>
             <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
             {props.ingredients.length < 4 && <h3>Please Enter Atleast 4 Ingredients to Get Started</h3>}
@@ -12,8 +20,26 @@ export default function IngredientsList(props) {
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button onClick={props.getRecipe}>Get a recipe</button>
+                {/* <button onClick={props.getRecipe}>Get a recipe</button> */}
+
+                <button onClick={handleGetRecipe} disabled={loading}>
+                Get a recipe
+                        {/* {loading ? "Loading..." : "Get a recipe"} */}
+                    </button>
+                    {/* {loading && <div className="spinner"></div>} */}
+
+
             </div>}
+            {loading && (
+                        <div className="frying-pan-loader">
+                            <div className="pan">
+                                <div className="handle"></div>
+                            </div>
+                            <div className="food food-1">🥕</div>
+                            <div className="food food-2">🥩</div>
+                            <div className="food food-3">🥓</div>
+                        </div>
+                    )}
         </section>
     )
 }
